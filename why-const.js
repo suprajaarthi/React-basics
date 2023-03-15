8fe437706ba2915dc191bfefa3c60169c03a7b1f
Supraja Arthi S <51900501+suprajaarthi@users.noreply.github.com>
2023-03-15 11:52:21 +0530

    Create

---
+++ why-const.js
@@ -0,0 +1,21 @@
+const answer1 = 42;
+
+/*
+	A big program here...
+*/
+
+answer1; // is still 42;
+
+
+
+// vs
+
+
+
+let answer2 = 42;
+
+/*
+	A big program here...
+*/
+
+answer2; // might have changed;
