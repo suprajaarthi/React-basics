57e048fbea626ade62bf93031883a762976d9edc
Supraja Arthi S <51900501+suprajaarthi@users.noreply.github.com>
2023-03-15 12:05:42 +0530

    Create

---
+++ destructuring.js
@@ -0,0 +1,25 @@
+// const PI = Math.PI;
+// const E = Math.E;
+// const SQRT2 = Math.SQRT2;
+
+const {PI, E, SQRT2}  = Math;
+
+// Somewhere in a React App
+// const {Component, Fragment, useState} = require('react');
+// useState();
+
+// const circle = {
+//   label: 'circleX',
+//   radius: 2,
+// };
+
+// const circleArea = ({radius}, {precision = 2} = {}) =>
+//   (PI * radius * radius).toFixed(precision);
+
+// console.log(
+//   circleArea(circle, { precision: 5 })
+// );
+
+// const [first, second,, forth] = [10, 20, 30, 40];
+
+// const [value, setValue] = useState(initialValue);
