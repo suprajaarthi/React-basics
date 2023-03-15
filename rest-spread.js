d32ee937b87ceb69fed93511c3f15b1fab907fe9
Supraja Arthi S <51900501+suprajaarthi@users.noreply.github.com>
2023-03-15 12:07:16 +0530

    rest-spread

---
+++ rest-spread.js
@@ -0,0 +1,19 @@
+const [first, ...restOfItems] = [10, 20, 30, 40];
+
+// console.log(first);
+// console.log(restOfItems);
+
+const data = {
+	temp1: '001',
+  temp2: '002',
+  firstName: 'John',
+  lastName: 'Doe',
+};
+
+const {temp1, temp2, ...person} = data;
+
+const newArray = [...restOfItems];
+
+const newObject = {
+  ...person
+}
