6516775b7fea824fac849489b0574ea85bef9ec1
Supraja Arthi S <51900501+suprajaarthi@users.noreply.github.com>
2023-03-15 12:03:26 +0530

    Create

---
+++ ObjLiterals.js
@@ -0,0 +1,19 @@
+/*
+      const obj = {
+        key: value
+      };
+*/
+
+const mystery = 'answer';
+const InverseOfPI = 1 / Math.PI;
+
+const obj = {
+	p1: 10,
+  p2: 20,
+  f1() {},
+  f2: () => {},
+  [mystery]: 42,
+  InverseOfPI,
+};
+
+console.log(obj);
